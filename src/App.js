d9b940c43a76f076f56e1e57f1730dc0eabc87f4
thangtuang <jhntuang1@gmail.com>
2023-04-29 16:26:27 -0400

    edited some texts

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Thang Tuang Web Developer!
         </a>
       </header>
