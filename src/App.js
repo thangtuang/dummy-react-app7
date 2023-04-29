694a2a790ac52307e17aa2f76ce96f1c60fa7a5d
thangtuang <jhntuang1@gmail.com>
2023-04-29 16:29:31 -0400

    deleted logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a
-          Thang Tuang Web Developer!
+          Thang Tuang Web Developer 
         </a>
       </header>
     </div>
